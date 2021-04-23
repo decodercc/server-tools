e0991340b3967a71799da81595b6dfb2cc808907
decodercc <densa.faradensa2019@gmail.com>
2021-04-23 19:28:03 +0300

    BrainyCP installer
    Added BrainyCP installer

---
+++ CentOS/BrainyCP/install.sh
@@ -0,0 +1,1 @@
+yum clean all && yum install -y wget && wget -O install.sh http://core.brainycp.com/install.sh && bash ./install.sh --package=apache2.4,nginx,php71w,bindserver,imagemagick,logrotate,MySql5.5,phpMyAdmin-4.9.4,exim,spamassassin,clamav,proftpd,csf
